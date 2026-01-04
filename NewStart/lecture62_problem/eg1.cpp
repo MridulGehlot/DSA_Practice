4e3dec9074f6e9c1d4a3b2948155db51497f59c4
MridulGehlot <mridulgehlot03@gmail.com>
2026-01-04 19:32:41 +0530

    some task

--- NewStart/lecture62_problem/eg1.cpp
+++ NewStart/lecture62_problem/eg1.cpp
@@ -6,16 +6,18 @@ int arr[20]={12,3,92,-3,76,54,12,93,78,45,56,23,89,51,20,88,30,44,62,10};
 int last;
 for(int i=0;i<=19;i++)
 {
+bool flag=false;
 last=arr[i];
 for(int j=i+1;j<=19;j++)
 {
 if(arr[j]>=last)
 {
+flag=true;
 cout<<last<<" ";
 last=arr[j];
 }
 }
-cout<<last<<endl;
+if(flag)cout<<last<<endl;
 }
 return 0;
 }
