cd22996423f66d1c8270930adb796a2204a6da02
MridulGehlot <mridulgehlot03@gmail.com>
2026-02-26 22:25:54 +0530

    Minimum Depth of BST

---
+++ NewStart/bst/eg10.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<iostream>
+#include<stack>
+#include<stdlib.h>
+using namespace std;
+typedef struct _BSTNode
+{
+int data;
+struct _BSTNode *left,*right;
+}BSTNode;
+BSTNode *root=NULL;
+BSTNode * createNode(int data)
+{
+BSTNode *n=(BSTNode *)malloc(sizeof(BSTNode));
+n->data=data;
+n->left=n->right=NULL;
+return n;
+}
+void insert(int data)
+{
+BSTNode *n,*t;
+n=createNode(data);
+if(root==NULL)
+{
+root=n;
+return;
+}
+t=root;
+while(1)
+{
+if(t->data<data)
+{
+if(t->right) t=t->right;
+else
+{
+t->right=n;
+break;
+}
+}
+else
+{
+if(t->left) t=t->left;
+else
+{
+t->left=n;
+break;
+}
+}
+}
+}
+int height(BSTNode *root)
+{
+if(!root) return 0;
+return min(height(root->left),height(root->right))+1;
+}
+int main()
+{
+insert(100);
+insert(50);
+insert(175);
+insert(75);
+insert(200);
+insert(10);
+insert(45);
+insert(170);
+insert(80);
+insert(20);
+int count=0;
+count=height(root);
+printf("Minimum Depth of Tree = %d\n",count);
+return 0;
+}
